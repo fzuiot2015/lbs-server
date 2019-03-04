0432eb03d99358d61f2b81f40aee942193d1a27e
ZengJinRong <jinrong.zeng@outlook.com>
2019-03-04 15:33:38 +0800

    增加心率计算过程

---
+++ src/main/java/edu/fzu/lbs/service/HeartBeatService.java
@@ -0,0 +1,50 @@
+package edu.fzu.lbs.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HeartBeatService {
+
+    private static final double THRESHOLD = 620.0;
+
+    /**
+     * 计算心率
+     *
+     * @param outputs 传感器输出
+     * @return
+     */
+    public int getHeartBeat(List<Integer> outputs) {
+        boolean belowThreshold = true;
+        List<Double> beats = new ArrayList<>();
+
+        for (Integer output : outputs) {
+            if (output > THRESHOLD && belowThreshold) {
+                double currentBPM = calculateBPM();
+                beats.add(currentBPM);
+                belowThreshold = false;
+            } else if (output < THRESHOLD) {
+                belowThreshold = true;
+            }
+        }
+
+        double average = beats.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+        return (int) average;
+    }
+
+    /**
+     * 计算当前心率
+     *
+     * @return 瞬时心率数值
+     */
+    private double calculateBPM() {
+        long newTime = System.currentTimeMillis();
+        long oldTime = 0;
+        double diff = newTime - oldTime;
+        double currentBPM = 60000 / diff;
+        return currentBPM;
+    }
+
+}
